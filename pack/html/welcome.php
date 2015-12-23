c3dfc747ac05a7d7d6d62d6e0429bdbdbfdcec15
Ting Liang <tingliang7t@foxmail.com>
2015-12-23 08:54:22 +0800

    pack

---
+++ pack/html/welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>none</title>
+</head>
+<body>
+Welcome <?php echo $_POST["fname"]; ?>!<br>
+You are <?php echo $_POST["age"]; ?> years old.
+</body>
+</html>
